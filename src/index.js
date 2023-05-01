33f44413ebc04a3d423b36eb57746f8de8826bf5
Dana <dana.lalovaod@gmail.com>
2023-05-01 21:34:53 +0300

    feat: loadmore listener

--- src/index.js
+++ src/index.js
@@ -2,55 +2,72 @@ import Notiflix from 'notiflix';
 
 
 const refs = {
-  form: document.querySelector('#search-form'),
-  searchQuery: document.querySelector('[name="searchQuery"]'),
-  searchBtn: document.querySelector('.submit-btn'),
-  loadMoreBtn: document.querySelector('.load-more'),
-  gallery: document.querySelector('.gallery'),
+    form: document.querySelector('#search-form'),
+    searchQuery: document.querySelector('[name="searchQuery"]'),
+    searchBtn: document.querySelector('.submit-btn'),
+    loadMoreBtn: document.querySelector('.load-more'),
+    gallery: document.querySelector('.gallery'),
 };
 
 
 const API_KEY = '13927284-11be6ffea1679f093739cb091';
 refs.form.addEventListener('submit', onSearchSubmit);
 let query = '';
+let markdown = '';
+let page = 1;
+let per_page = 40;
+
+
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearchSubmit(event) {
     event.preventDefault();
-    if (query){
+    if (query) {
         console.log('erase previous data');
-        query='';
-        refs.gallery.innerHTML='';
+        query = '';
+        refs.gallery.innerHTML = '';
+        return;
     }
-    
-  query = refs.searchQuery.value;
-  console.log(query);
 
-  fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
-    .then(responce => responce.json())
-    .then(images => {
-        if (images.hits.length===0){
-            Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-            refs.form.reset();
-            return;
-        }
-        let markdown='';
+    page = 1;
+    query = refs.searchQuery.value;
 
-        const imagesArray = images.hits.map(image =>{
-            const imageMarkDown = makeMarkdown(image);
-            markdown +=imageMarkDown;
+    let URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`;
 
+    fetch(URL)
+        .then(responce => responce.json())
+        .then(images => {
+            if (images.hits.length === 0) {
+                Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+                refs.form.reset();
+                return;
+            }
+
+            //сколько страниц приходит
+            let totalPages = images.totalHits / per_page;
+            console.log('totalPages', images.totalHits, totalPages);
+
+            //показывать ли load more
+            if (totalPages > 1) {
+                refs.loadMoreBtn.style.visibility = "visible";
+            }
+
+
+            const imagesArray = images.hits.map(image => {
+                const imageMarkDown = makeMarkdown(image);
+                markdown += imageMarkDown;
+            })
+
+            refs.gallery.insertAdjacentHTML('afterbegin', markdown);
 
         })
-
-        refs.gallery.insertAdjacentHTML('afterbegin', markdown);
-
-    })
-    .catch(error => console.log(error));
+        .catch(error => console.log(error));
 }
 
 
+// function fetchItems (url){}
 
-function makeMarkdown (item){
+function makeMarkdown(item) {
     return `<div class="photo-card">
     <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
     <div class="info">
@@ -73,3 +90,19 @@ function makeMarkdown (item){
     </div>
   </div>`;
 }
+
+function onLoadMore(event) {
+    console.log('click on load more');
+    page += 1;
+
+    fetch(URL).then(responce => responce.json()).then(images => {
+        const imagesArray = images.hits.map(image => {
+            const imageMarkDown = makeMarkdown(image);
+            markdown += imageMarkDown;
+       
+        }).catch(error => console.log(error));
+        refs.gallery.insertAdjacentHTML('afterbegin', markdown);
+
+
+    }
+)}
